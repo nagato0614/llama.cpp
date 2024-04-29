b5d0814b5cc0bdd6704899b9a431ec5413c8fec8
Toru Nagai <nagato0614@gmail.com>
2024-04-29 19:14:06 +0900

    🐛 修正(convert_parquet_to_txt.py): テキストファイルの保存先パスを修正し、parquetファイルを正しいディレクトリに変換して保存するように修正 ✨ 新機能(convert_parquet_to_txt.py): parquetファイルをtxtファイルに変換する処理を追加

--- convert_parquet_to_txt.py
+++ convert_parquet_to_txt.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 def main():
     # パスの指定
-    parquet_path = '/Volumes/SSPA-USC/train_parquet/'
-    txt_path = '/Volumes/SSPA-USC/train_txt/train.txt'
+    parquet_path = '/Volumes/SSPA-USC/train_parquet/train_wiki/'
+    txt_path = '/Volumes/SSPA-USC/train_txt/'
 
     # 対象のディレクトリいかにあるparquetファイル一覧を取得
     parquet_files = []
@@ -20,25 +20,21 @@ def main():
     parquet_files = [parquet_file for parquet_file in parquet_files if '._' not in parquet_file]
     print(parquet_files)
 
+    # 一つずつparquetファイルを読み込んで、それぞれをtxtファイルに変換
+    for parquet_file in parquet_files:
+        print(f'## {parquet_file}')
+        # parquetファイルを読み込む
+        df = pd.read_parquet(parquet_file)
 
+        # 書き込むファイル名を作成
+        txt_file = parquet_file.replace(parquet_path, txt_path).replace('.parquet', '.txt')
+        print(f'## {txt_file}')
 
-    with open(txt_path, 'w') as f:
-        for parquet_file in parquet_files:
-            print(f'## {parquet_file}')
-            # Parquet からロード
-            df = pd.read_parquet(parquet_file)
-
-            # 各行の改行文字を削除
-            df = df.replace('\n', '', regex=True)
-
+        # ファイルを開く
+        with open(txt_file, 'w') as f:
             # テキストファイルに追記
             for i in range(len(df)):
                 f.write(df.iloc[i]['text'] + '\n')
-
-            break
-
-
-
 
 if __name__ == '__main__':
     main()
